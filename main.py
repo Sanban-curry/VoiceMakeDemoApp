55a8c7ae60809b39ac021c994a056663c228dc72
sanban.curry@gmail.com <canban.curry@gmail.com>
2022-11-10 18:34:26 +0900

    4th commit

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
 import os
 
-from google.cloud import texttospeech
-
-
-import synthezise_voice
-
+import View
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'secret.json'
-
-
-gender = 'default'
-lang = 'English'
-text = "Hi, I am goodlooking genius guy.。"
-
 def main():
-
-
-    response = synthezise_voice.synthesize_speech(text, lang, gender)
-
-    file_name = 'output.mp3'
-    with open(file_name, "wb") as out:
-        out.write(response.audio_content)
-        print('Audio content written to file "output.mp3"')
-
+    View.View()
 
 if __name__ == '__main__':
     main()
-
